82e43bbeb5046027d63942c3bbd57baeed0e2ecc
Julia <juliamirandabrito@gmail.com>
2022-11-11 11:50:33 -0300

    imc

--- imc.c
+++ imc.c
@@ -8,24 +8,36 @@
 
 int main(void){
     float altura, imc;
-    float peso;
+    int peso;
 
     printf("Qual seu peso, em kg, e sua altura, em m? ");
-    scanf("%f %f\n", &peso, &altura);
-    printf("%f %f\n", peso, altura);
+    scanf("%d %f", &peso, &altura);
 
     imc = peso/(altura * altura);
-    printf("%.2f\n", imc);
 
+    if(imc < 16)
+        printf("%.2f (Perigo de vida)", imc);
 
+    if(imc >= 16 && imc < 17)
+        printf("%.2f (Muito abaixo do peso)", imc);
 
+    if(imc >= 17 && imc < 18.5)
+        printf("%.2f (Abaixo do peso)", imc);
 
+    if(imc >= 18.5 && imc < 25)
+        printf("%.2f (Peso normal)", imc);
 
-    // printf("Para um peso de 60,0kg e uma altura de 1,60m, temos um IMC = 23,44 que equivale a Peso normal. %.2f", imc);
+    if(imc >= 25 && imc < 30)
+        printf("%.2f (Acima do peso)", imc);
 
-    
+    if(imc >= 30 && imc < 35)
+        printf("%.2f (Obesidade grau I)", imc);
 
+    if(imc >= 35 && imc < 40)
+        printf("%.2f (Obesidade grau II)", imc);
 
+    if(imc >= 40)
+        printf("%.2f (Obesidade grau III)", imc);
 
 
     return 0;
