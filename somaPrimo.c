8d836ed60726812051fa140af3163e17b18fc34d
Julia <juliamirandabrito@gmail.com>
2022-11-27 11:50:06 -0300

    somaPrimo.c

--- somaPrimo.c
+++ somaPrimo.c
@@ -6,30 +6,24 @@
 
 #include <stdio.h>
 
-
-int ehPrimo(int n);
-
 int main(void){
     int n, a, b;
     int soma = 0;
     printf("Digite a e b do intervalo [a,b], sendo a < b: ");
     scanf("%d %d", &a, &b);
     if (a < b){
-        for(n = a; n <= b; n++){ 
-            if (ehPrimo (n) == n){
-                soma = soma + n;
-            } 
-        } 
+        for(n = a; n <= b; n++){
+            for (int i = 2; i < n; i++) {
+                if(n % i != 0) {
+                    soma = soma + n;
+                }
+            }
+                
+        }
     }
     printf("%d ", soma);
     return 0;
 }
 
-int ehPrimo(int n){
-    for (int i = 2; i < n; i++) {
-        if(n % i != 0) {
-            return n;
-        }
-    };
-}
+
 
