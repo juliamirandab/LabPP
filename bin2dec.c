9d3bd7bbb2d7df41c0c6b45095020c17016ba617
Julia <juliamirandabrito@gmail.com>
2022-12-12 10:12:24 -0300

    bin2dec.c

--- bin2dec.c
+++ bin2dec.c
@@ -4,4 +4,32 @@
  * Autor: Júlia Miranda 
  */
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
+#define N 1024
+void getstr(char * str, long long int nchar);
+int main(void) {
+    char bin[N] = {'x','x','x','x','x'};
+    printf("Entre com um numero binario: ");
+    getstr(bin, N);
+    long long int dec = 0, i = 0;
+    long long int len = strlen(bin);
 
+    while(len--){
+        if(bin[len] == '0' || bin[len] == '1'){
+            dec = dec + pow(2, i++) * (bin[len] - '0');
+        }
+    }
+    printf("%llu", dec);
+
+    return 0;
+}    
+void getstr(char * str, long long int nchar) {
+    char c;
+    long long int i;
+    for(i = 0; i < nchar && (c = getchar()) != '\n'; i++)
+        str[i] = c;
+    str[(i >= nchar) ? nchar - 1 : i] = '\0';
+    if(i >= nchar) // precisa limpar o buffer do teclado
+        while ((c = getchar()) != '\n' && c != EOF);
+}
