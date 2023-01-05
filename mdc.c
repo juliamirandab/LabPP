60577daaa6b152423cdb537922550942d1e78484
Julia <juliamirandabrito@gmail.com>
2023-01-05 09:25:11 -0300

    mdc.c

---
+++ mdc.c
@@ -0,0 +1,23 @@
+/* 
+ * Arquivo: mdc.c
+ * Data de criação: 05/01/22
+ * Autor: Júlia Miranda 
+ */
+#include <stdio.h>
+int mdc(int a, int b);
+int main(void){
+    printf("Digite dois numeros: ");
+    int a, b, x;
+    scanf("%d %d", &a, &b);
+    x = mdc(a, b);
+    printf("%d", x);
+    return 0;
+}
+
+int mdc(int a, int b){
+    if (b == 0){
+        return a;
+    } else{
+        return mdc(b, a%b);
+    }
+}
