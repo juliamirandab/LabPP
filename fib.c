9764b5297c0fc4654111b72dfa329696623fe1c6
Julia <juliamirandabrito@gmail.com>
2023-01-05 09:38:39 -0300

    fib.c

---
+++ fib.c
@@ -0,0 +1,24 @@
+/* 
+ * Arquivo: fib.c
+ * Data de criação: 05/01/22
+ * Autor: Júlia Miranda 
+ */
+#include <stdio.h>
+int fibonacci(int n);
+int main(void){
+    int a, x;
+    printf("Digite um numero: ");
+    scanf("%d", &a);
+    x = fibonacci(a);
+    printf("%d", x);
+    return 0;
+}
+int fibonacci(int n){
+    if(n == 0){
+        return 0;
+    }if(n == 1 || n == 2){
+        return 1;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+    
+}
