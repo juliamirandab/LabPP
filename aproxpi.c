3b4965d672289ee0471453c0fd719e3d1e40dfe4
Julia <juliamirandabrito@gmail.com>
2023-01-07 15:16:35 -0300

    aproxipi.c

--- aproxpi.c
+++ aproxpi.c
@@ -1,22 +1,22 @@
 /* 
  * Arquivo: aproxpi.c
- * Data de criação: 05/01/22
+ * Data de criação: 05/01/23
  * Autor: Júlia Miranda 
  */
 #include <stdio.h>
 #include <math.h>
-int aproximaPi(int n);
+double aproximaPi(int n);
 int main(void){
     int n;
-    float x;
+    double x;
     printf("Digite o limite superior da serie: ");
     scanf("%d", &n);
     x = aproximaPi(n);
-    printf("%g", x);
+    printf("%lf\n", x);
     return 0;
 }
-int aproximaPi(int n){
+double aproximaPi(int n){
     if (n == 0) 
-        return 4;
-    return aproximaPi(n - 1) + 4 * (pow(-1, n))/(2*n + 1);
+        return 4.0;
+    return (aproximaPi(n - 1) + (4 * ((pow(-1, n))/((2*n) + 1))));
 }
