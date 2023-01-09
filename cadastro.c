9527860ce2a523dcee452a90a54b692bb46bb74b
Julia <juliamirandabrito@gmail.com>
2023-01-08 22:14:49 -0300

    cadastro.c

--- cadastro.c
+++ cadastro.c
@@ -1,6 +1,6 @@
 /* 
  * Arquivo: cadastro.c
- * Data de criação: 07/01/23
+ * Data de criação: 08/01/23
  * Autor: Júlia Miranda 
  */
 #include <stdio.h>
@@ -34,21 +34,20 @@ int main(void){
     Data data;
     Altura altura;
     Peso peso;
-    int n;
+    int n, i = 0;
     printf("Cadastro para quantas pessoas? ");
     scanf("%d", &n);
-    for(int i = 0; i<n; i++){
-        printf("Digite seu nome, sua data de nascimento(DD/MM/YY), sua altura e seu peso: ");
+    printf("Digite o nome, a data de nascimento(DD/MM/YY), a altura e o peso: ");
+    while(i != n){
         scanf("%s %s %d/%d/%d %d.%d %f",
         &nome.nome, &nome.sobrenome, &data.dia, &data.mes, &data.ano, &altura.metro, &altura.centimetro, &peso.kg);
         altura.centimetro = cm(altura.centimetro);
         printf("%s %s; %02d%s%04d; Altura %dm%d; %0.1fkg\n",
         nome.nome, nome.sobrenome, data.dia, strmes[data.mes], data.ano, altura.metro, altura.centimetro, peso.kg);
-        
+        i++;
     }
-   
-    return 0;
 
+    return 0;
 }
 int cm(int x){
     if(x < 10) return x*10;
